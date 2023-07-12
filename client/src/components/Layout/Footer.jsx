52e117f9ba224c50b8b149cdf4af1ceb482508a9
Abdul Mateen <mateenabdul993@gmail.com>
2023-07-12 08:16:29 +0500

    Setting up frontend initially

---
+++ client/src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer= ()  => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="bg-slate-200 rounded-md  text-center px-10 mt-2   ">
+        <h6>All Rights Reserved &copy; Abdul Mateen</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -79,3 +79,4 @@ export const signup = async (req,res) => {
     }
 
 }
+
